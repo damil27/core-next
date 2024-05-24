343be1d6aa8f520e93d267eef06c2f2d49f5bec9
damin <34189426+damil27@users.noreply.github.com>
2024-05-24 10:08:52 +0100

    styles active link in the navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,22 +1,34 @@
+"use client"
+
 import Link from "next/link";
 import React from "react";
 import { FaBug } from "react-icons/fa";
+import classNames from "classNames"
+
+
+import { usePathname } from "next/navigation";
 
 const NavBar = () => {
     const links = [
         {label: "Dashboard", href:"/"},
-        {label: "Issue", href:"/"},
+        {label: "Issue", href:"/issue"},
     ]
+   const currentPath  =  usePathname()
   return (
     <nav className="flex border-b space-x-6 h-14 items-center p-8 mb-5">
       <Link href="/">
         <FaBug />
       </Link>
       <ul className="flex space-x-6">
-              {links.map(link => <Link key={link.label} href={link.href} className=" text-zinc-500 hover:text-zinc-800 transition-colors" >{link.label }</Link>)}
+              {links.map(link => <Link key={link.label} href={link.href} className={classNames({
+                  "text-zinc-900": link.href === currentPath,
+                  "text-zinc-500": link.href !== currentPath,
+                  "hover:text-zinc-800 transition-colors":true
+              })} >{link.label }</Link>)}
       </ul>
     </nav>
   );
 };
 
 export default NavBar;
+" text-zinc-500 hover:text-zinc-800 transition-colors"
---
+++ app/issue/page.tsx
@@ -0,0 +1,9 @@
+function IssuePage() {
+  return (
+    <div>
+      <h1>Issue page</h1>
+    </div>
+  );
+}
+
+export default IssuePage;
